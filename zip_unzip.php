4b0c7009bfd60ea6907540508ef7bd1e91a63829
Shakhawat Hossain <shakhawat.flickmedia@gmail.com>
2023-04-20 14:39:32 +0600

    Create zip_unzip.php
    Unzip File

---
+++ zip_unzip.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * ZIP All content of current folder
+ * @link https://shellcreeper.com/?p=1249
+ */
+ 
+/* ZIP File name and path */
+$zip_file = 'files.zip';
+ 
+/* Exclude Files */
+$exclude_files = array();
+$exclude_files[] = realpath( $zip_file );
+$exclude_files[] = realpath( 'zip.php' );
+ 
+/* Path of current folder, need empty or null param for current folder */
+$root_path = realpath( '' );
+ 
+/* Initialize archive object */
+$zip = new ZipArchive;
+$zip_open = $zip->open( $zip_file, ZipArchive::CREATE );
+ 
+/* Create recursive files list */
+$files = new RecursiveIteratorIterator(
+    new RecursiveDirectoryIterator( $root_path ),
+    RecursiveIteratorIterator::LEAVES_ONLY
+);
+ 
+/* For each files, get each path and add it in zip */
+if( !empty( $files ) ){
+ 
+    foreach( $files as $name => $file ) {
+ 
+        /* get path of the file */
+        $file_path = $file->getRealPath();
+ 
+        /* only if it's a file and not directory, and not excluded. */
+        if( !is_dir( $file_path ) && !in_array( $file_path, $exclude_files ) ){
+ 
+            /* get relative path */
+            $file_relative_path = str_replace( $root_path, '', $file_path );
+ 
+            /* Add file to zip archive */
+            $zip_addfile = $zip->addFile( $file_path, $file_relative_path );
+        }
+    }
+}
+ 
+/* Create ZIP after closing the object. */
+$zip_close = $zip->close();
+?>
+
+UNZIP Files using PHP
+
+<?php
+/**
+ * Unzip File in the same directory.
+ * @link http://stackoverflow.com/questions/8889025/unzip-a-file-with-php
+ */
+$file = 'file.zip';
+ 
+$path = pathinfo( realpath( $file ), PATHINFO_DIRNAME );
+ 
+$zip = new ZipArchive;
+$res = $zip->open($file);
+if ($res === TRUE) {
+    $zip->extractTo( $path );
+    $zip->close();
+    echo "WOOT! $file extracted to $path";
+}
+else {
+    echo "Doh! I couldn't open $file";
+}
+?>
